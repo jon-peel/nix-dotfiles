2efb986e418aaf94e3ed6068ddc64f6890918922
Jonathan Peel <me@jonathanpeel.co.za>
2025-10-23 15:40:09 +0000

    feat: change to p10k zsh prompt

--- homeManagerModules/my-zsh.nix
+++ homeManagerModules/my-zsh.nix
@@ -5,25 +5,25 @@
   ...
 }:
 
-{
+let powerlevel10k = {
+      name = "powerlevel10k";
+      src = pkgs.zsh-powerlevel10k;
+      file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    };
+
+in {
   options = {
     my-zsh.enable = lib.mkEnableOption "enable ZSH config";
   };
 
   config = lib.mkIf config.my-zsh.enable {
+    home.file.".p10k.zsh".source = ./files/p10k.zsh;
+
     programs.zsh = {
       enable = true;
       enableCompletion = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
-
-      # Powerline configuration
-      initContent = ''
-        # Powerline
-        if [ -f ${pkgs.powerline}/share/zsh/site-functions/_powerline_prompt ]; then
-          source ${pkgs.powerline}/share/zsh/site-functions/_powerline_prompt
-        fi
-      '';
 
       oh-my-zsh = {
         enable = true;
@@ -33,8 +33,14 @@
           "podman"
           "kubectl"
         ];
-        theme = "robbyrussell"; # You can change this
+        theme = "";
       };
+
+      plugins = [ powerlevel10k ];
+      initExtra = ''
+        # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+      '';
 
       shellAliases = {
         ll = "ls -l";
